55a7ed8d301745e348a8706edaf17cb80cd711e9
PeachP <84045459+PeachPaulison@users.noreply.github.com>
2021-06-19 20:18:33 +0000

    Part 1 of Candidate Testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,11 +3,11 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
+let candidateName = ("");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let question = ("Who was the first woman in Space? ");
+let correctAnswer = ("Sally Ride");
+let candidateAnswer = ("");
 let questions;
 let correctAnswers;
 let candidateAnswers;
@@ -15,18 +15,26 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+const input1 = require('readline-sync');
+candidateName = String(input1.question("Please enter your full name: "));
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-
+const input2 = require('readline-sync');
+candidateAnswer = String((input2.question(question)));
+console.log(candidateAnswer)
 }
+
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+if (correctAnswer === candidateAnswer) {
+console.log("Correct!");
+} else if (correctAnswer !== candidateAnswer) {
+  console.log("Incorrect");} 
 
 
   let grade;
@@ -38,7 +46,7 @@ function gradeQuiz(candidateAnswers) {
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log("Welcome, " + candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
