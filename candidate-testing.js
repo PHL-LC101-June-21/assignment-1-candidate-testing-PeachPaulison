6cd1538c91de91e86c2eb65948cf8161f4fa6154
PeachP <84045459+PeachPaulison@users.noreply.github.com>
2021-06-22 02:54:33 +0000

    Created arrays for questions and correctAnswers

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,3 +1,5 @@
+//I'm stuck... I'm bleary eyed and the only thing I can think to make this run is too cludgey, defeats the purpose of the array and can't be correct.   I will just commit what I have to GitHub and submit it even though I know it no longer runs after I started Part 2. I'll continue to work on it.// 
+
 const input = require('readline-sync');
 
 // TODO 2: modify your quiz app to ask 5 questions //
@@ -5,12 +7,21 @@ const input = require('readline-sync');
 // TODO 1.1a: Define candidateName // 
 let candidateName = ("");
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question = ("Who was the first woman in Space? ");
-let correctAnswer = ("Sally Ride");
-let candidateAnswer = ("");
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", "True or False: 5 kilometers == 5000 meters? ", "(5 + 3)/ 2 * 10 = ", "Given the array [8, 'Orbit', 'Trajectory', 45] what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
+// console.log(questions)
+
+let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
+// let question = questions[0];
+// let question = questions[1];
+// let question = questions[2];
+// let question = questions[3];
+// let question = questions[4];
+// let question = ("Who was the first woman in Space? ");
+let correctAnswer = correctAnswers[0];
+// let correctAnswer = ("Sally Ride");
+// let candidateAnswer = ("");
+
+let candidateAnswers = [];
 
 
 function askForName() {
@@ -22,9 +33,12 @@ candidateName = String(input1.question("Please enter your full name: "));
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
+ //const input2 = require('readline-sync');
+// candidateAnswer = String((input2.question(questions[0])));
+// console.log(candidateAnswer)
 const input2 = require('readline-sync');
-candidateAnswer = String((input2.question(question)));
-console.log(candidateAnswer)
+candidateAnswers = String((input2.question(questions[0])));
+console.log(candidateAnswers)
 }
 
 
